078d33f39f4e5aff7e2e9bba8c0e3c5b2a7e1342
Andrielson Silva <andrielson@gmail.com>
2022-08-20 18:08:11 -0300

    Implement get author by id

--- src/main/java/com/github/andrielson/sdjpahibernatedao/dao/AuthorDaoImpl.java
+++ src/main/java/com/github/andrielson/sdjpahibernatedao/dao/AuthorDaoImpl.java
@@ -1,13 +1,23 @@
 package com.github.andrielson.sdjpahibernatedao.dao;
 
 import com.github.andrielson.sdjpahibernatedao.domain.Author;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
+
+    private final EntityManagerFactory emf;
+
+    public AuthorDaoImpl(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+
     @Override
     public Author getById(Long id) {
-        return null;
+        return getEntityManager().find(Author.class, id);
     }
 
     @Override
@@ -28,5 +38,9 @@ public class AuthorDaoImpl implements AuthorDao {
     @Override
     public void deleteAuthorById(Long id) {
 
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
     }
 }
