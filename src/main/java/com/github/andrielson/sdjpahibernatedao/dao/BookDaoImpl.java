4ba0c7b532bffad36e3e0f5ade353fd584b66046
Andrielson Silva <andrielson@gmail.com>
2022-08-20 18:52:13 -0300

    Add BookDao implementation

---
+++ src/main/java/com/github/andrielson/sdjpahibernatedao/dao/BookDaoImpl.java
@@ -0,0 +1,47 @@
+package com.github.andrielson.sdjpahibernatedao.dao;
+
+import com.github.andrielson.sdjpahibernatedao.domain.Book;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookDaoImpl implements BookDao {
+
+    private final EntityManagerFactory emf;
+
+    public BookDaoImpl(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public Book getById(Long id) {
+        return getEntityManager().find(Book.class, id);
+    }
+
+    @Override
+    public Book findBookByTitle(String title) {
+        var query = getEntityManager().createQuery("SELECT b FROM Book b WHERE b.title = :title", Book.class);
+        query.setParameter("title", title);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public Book saveNewBook(Book Book) {
+        return null;
+    }
+
+    @Override
+    public Book updateBook(Book Book) {
+        return null;
+    }
+
+    @Override
+    public void deleteBookById(Long id) {
+
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+}
